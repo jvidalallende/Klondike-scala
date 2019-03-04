6c5f7c232a3fd189f6907fd9079f5989af1302fe
Juan Vidal Allende <juanvidalallende@gmail.com>
2019-03-04 19:56:40 +0100

    Use partial function to reuse code in movements

--- src/main/scala/controllers/MovementFactory.scala
+++ src/main/scala/controllers/MovementFactory.scala
@@ -7,69 +7,51 @@ import models.Pile
    f(Pile, Pile, Int) --> (Pile, Pile) */
 class MovementFactory {
 
-  def deckToWaste(): (Pile, Pile, Int) => (Pile, Pile) = {
-    def move(deck: Pile, waste: Pile, numberOfCards: Int = 1): (Pile, Pile) = {
-      numberOfCards match {
-        case 0 => (deck, waste)
-        case _ =>
-          val pickResult = deck.pick()
-          move(pickResult._2, waste.put(pickResult._1), numberOfCards - 1)
-      }
+  private def move(validator: (Pile, Pile) => Boolean)(source: Pile, destination: Pile, numberOfCards: Int = 1)
+  : (Pile, Pile) = {
+    numberOfCards match {
+      case 0 => (source, destination)
+      case _ =>
+        if (!validator(source, destination)) {
+          throw InvalidMoveException()
+        }
+        val pickResult = source.pick()
+        move(validator)(pickResult._2, destination.put(pickResult._1), numberOfCards - 1)
     }
+  }
 
-    move
+  def deckToWaste(): (Pile, Pile, Int) => (Pile, Pile) = {
+    def validator(deck: Pile, waste: Pile): Boolean = true
+
+    move(validator)
   }
 
   def wasteToFoundation(): (Pile, Pile, Int) => (Pile, Pile) = {
-    def validMove(waste: Pile, tableauPile: Pile): Boolean = {
+    def validator(waste: Pile, foundation: Pile): Boolean = {
       val topOfWaste = waste.pick()._1
-      tableauPile match {
+      foundation match {
         case f if f.empty() => topOfWaste.isMin()
         case _ =>
-          val topOfFoundation = tableauPile.pick()._1
+          val topOfFoundation = foundation.pick()._1
           (topOfWaste.suit() == topOfFoundation.suit()) && (topOfWaste.value() - topOfFoundation.value() == 1)
       }
     }
 
-    def move(waste: Pile, tableauPile: Pile, numberOfCards: Int = 1): (Pile, Pile) = {
-      numberOfCards match {
-        case 0 => (waste, tableauPile)
-        case _ =>
-          if (!validMove(waste, tableauPile)) {
-            throw InvalidMoveException()
-          }
-          val pickResult = waste.pick()
-          move(pickResult._2, tableauPile.put(pickResult._1), numberOfCards - 1)
-      }
-    }
-
-    move
+    move(validator)
   }
 
   def wasteToTableauPile(): (Pile, Pile, Int) => (Pile, Pile) = {
-    def validMove(waste: Pile, tableauPile: Pile): Boolean = {
+    def validator(waste: Pile, tableauPile: Pile): Boolean = {
       val topOfWaste = waste.pick()._1
       tableauPile match {
         case f if f.empty() => topOfWaste.isMax()
         case _ =>
           val topOfTableauPile = tableauPile.pick()._1
-          //TODO: consider different stacking rules
+          //TODO: consider supporting different stacking rules
           (topOfWaste.suit() != topOfTableauPile.suit()) && (topOfTableauPile.value() - topOfWaste.value() == 1)
       }
     }
 
-    def move(waste: Pile, tableauPile: Pile, numberOfCards: Int = 1): (Pile, Pile) = {
-      numberOfCards match {
-        case 0 => (waste, tableauPile)
-        case _ =>
-          if (!validMove(waste, tableauPile)) {
-            throw InvalidMoveException()
-          }
-          val pickResult = waste.pick()
-          move(pickResult._2, tableauPile.put(pickResult._1), numberOfCards - 1)
-      }
-    }
-
-    move
+    move(validator)
   }
 }
