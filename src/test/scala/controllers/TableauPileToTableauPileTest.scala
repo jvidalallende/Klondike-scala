bdcf6c3c6ecf98ce466f23289edafcdfe06abb9a
Juan Vidal Allende <juanvidalallende@gmail.com>
2019-03-05 20:01:18 +0100

    Add more tests for movement between tableau piles

--- src/test/scala/controllers/TableauPileToTableauPileTest.scala
+++ src/test/scala/controllers/TableauPileToTableauPileTest.scala
@@ -10,6 +10,8 @@ class TableauPileToTableauPileTest extends FunSuite {
 
   private val aceOfGolds = new SpanishCard(1, "golds", true)
   private val twoOfClubs = new SpanishCard(2, "clubs", true)
+  private val threeOfSwords = new SpanishCard(3, "swords", true)
+  private val fourOfCups = new SpanishCard(4, "cups", true)
   private val kingOfSwords = new SpanishCard(SpanishCard.MAX_VALUE, "swords", true)
 
   private val move = new MovementFactory().tableauPileToTableauPile()
@@ -20,33 +22,55 @@ class TableauPileToTableauPileTest extends FunSuite {
     }
   }
 
+  test("givenTwoEmptyTableauPiles_whenMovingZeroCardsFromBetweenThem_thenNoExceptionIsThrown") {
+    val (sourceAfterMove, destinationAfterMove) = move(emptyTableauPile, emptyTableauPile, 0)
+    assert(sourceAfterMove.empty())
+    assert(destinationAfterMove.empty())
+  }
+
   test("givenATableauPileWithAKing_whenMovingToAnEmptyTableauPile_thenTheFirstTableauIsEmptyAndTheOtherTableauPileContainsTheCard") {
-    val waste = new TableauPile(kingOfSwords :: Nil)
-    val result = move(waste, emptyTableauPile, 1)
-    assert(result._1.empty())
-    assert(kingOfSwords :: Nil == result._2.cards())
+    val source = new TableauPile(kingOfSwords :: Nil)
+    val (sourceAfterMove, destinationAfterMove) = move(source, emptyTableauPile, 1)
+    assert(sourceAfterMove.empty())
+    assert(kingOfSwords :: Nil == destinationAfterMove.cards())
   }
 
   test("givenATableauPileWithACardDifferentFromAKing_whenMovingToAnEmptyTableauPile_thenExceptionIsThrown") {
-    val waste = new TableauPile(aceOfGolds :: Nil)
+    val source = new TableauPile(aceOfGolds :: Nil)
     intercept[InvalidMoveException] {
-      move(waste, emptyTableauPile, 1)
+      move(source, emptyTableauPile, 1)
     }
   }
 
   test("givenATableauPileWithOneCard_whenMovingToATableauPileThatCanReceiveThatCard_thenNewTableauPileIsEmptyAndTableauPilesContainsTheCards") {
-    val waste = new TableauPile(aceOfGolds :: Nil)
-    val tableauPile = new TableauPile(twoOfClubs :: Nil)
-    val result = move(waste, tableauPile, 1)
-    assert(result._1.empty())
-    assert(aceOfGolds :: tableauPile.cards() == result._2.cards())
+    val source = new TableauPile(aceOfGolds :: Nil)
+    val destination = new TableauPile(twoOfClubs :: Nil)
+    val (sourceAfterMove, destinationAfterMove) = move(source, destination, 1)
+    assert(sourceAfterMove.empty())
+    assert(aceOfGolds :: destination.cards() == destinationAfterMove.cards())
   }
 
   test("givenATableauPileWithACard_whenMovingToATableauPileThatCannotReceiveThatCard_thenExceptionIsThrown") {
-    val waste = new TableauPile(twoOfClubs :: Nil)
-    val tableauPile = new TableauPile(aceOfGolds :: Nil)
+    val source = new TableauPile(twoOfClubs :: Nil)
+    val destination = new TableauPile(aceOfGolds :: Nil)
     intercept[InvalidMoveException] {
-      move(waste, tableauPile, 1)
+      move(source, destination, 1)
+    }
+  }
+
+  test("givenATableauPileWithThreeCards_whenMovingThemToATableauPileThatCanReceiveThatCard_thenNewTableauPileIsEmptyAndTableauPilesContainsTheCards") {
+    val source = new TableauPile(aceOfGolds :: twoOfClubs :: threeOfSwords :: Nil)
+    val destination = new TableauPile(fourOfCups :: Nil)
+    val (sourceAfterMove, destinationAfterMove) = move(source, destination, 3)
+    assert(sourceAfterMove.empty())
+    assert(source.cards() ::: destination.cards() == destinationAfterMove.cards())
+  }
+
+  test("givenATableauPileWithThreeCards_whenMovingThemToATableauPileThatCannotReceiveThatCard_thenExceptionIsThrown") {
+    val source = new TableauPile(aceOfGolds :: twoOfClubs :: threeOfSwords :: Nil)
+    val destination = new TableauPile(kingOfSwords :: Nil)
+    intercept[InvalidMoveException] {
+      move(source, destination, 3)
     }
   }
 }
