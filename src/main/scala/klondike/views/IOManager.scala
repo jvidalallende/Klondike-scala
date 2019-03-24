7d9bb0dba5701ffff1ee3c7ad236bf623726bdba
Juan Vidal Allende <juanvidalallende@gmail.com>
2019-03-24 12:03:25 +0100

    Simplify IOManager to only contain the methods used

--- src/main/scala/klondike/views/IOManager.scala
+++ src/main/scala/klondike/views/IOManager.scala
@@ -2,11 +2,7 @@ package klondike.views
 
 abstract class IOManager {
 
-  def readString(title: String): String
-
   def readInt(title: String): Int
 
   def write(string: String): Unit
-
-  def write(value: Int): Unit
 }
--- src/main/scala/klondike/views/RealIOManager.scala
+++ src/main/scala/klondike/views/RealIOManager.scala
@@ -2,18 +2,11 @@ package klondike.views
 
 object RealIOManager extends IOManager {
 
-  override def readString(title: String): String = {
-    print(s"$title: ")
-    scala.io.StdIn.readLine()
-  }
-
   override def readInt(title: String): Int = {
-    readString(title).toInt
+    print(s"$title: ")
+    scala.io.StdIn.readLine().toInt
   }
 
   override def write(string: String): Unit =
     print(string)
-
-  override def write(value: Int): Unit =
-    print(value)
 }
