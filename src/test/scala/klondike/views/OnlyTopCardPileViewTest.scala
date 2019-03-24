b3d06624b0784d5dde7cfa964fadb6fbaf939a33
Juan Vidal Allende <juanvidalallende@gmail.com>
2019-03-24 20:22:17 +0100

    Extend OnlyTopCardPileViewTest to use French Cards

--- src/test/scala/klondike/views/OnlyTopCardPileViewTest.scala
+++ src/test/scala/klondike/views/OnlyTopCardPileViewTest.scala
@@ -1,22 +1,32 @@
 package klondike.views
 
 import klondike.test_utils.TestModels._
-import klondike.models.{Deck, Foundation, SpanishCard, Waste}
+import klondike.models.{Deck, Foundation, Waste}
 import org.scalatest.FunSuite
 
 class OnlyTopCardPileViewTest extends FunSuite with OnlyTopCardPileViewBehaviors {
 
-  private val aceOfGolds = new SpanishCard(1, "golds")
+  testsFor(checkView(emptyDeck, SpanishCardView, "[         ]", "withNoSpanishCards"))
+  testsFor(checkView(deckWithCard(aceOfGolds), SpanishCardView, "[XXXXXXXXX]", "withOneSpanishCard"))
+  testsFor(checkView(new Deck(aceOfGolds :: twoOfCups :: Nil), SpanishCardView, "[XXXXXXXXX]", "withMoreThanOneSpanishCard"))
 
-  testsFor(checkView(emptyDeck, SpanishCardView, "[         ]", "withNoCards"))
-  testsFor(checkView(deckWithCard(aceOfGolds), SpanishCardView, "[XXXXXXXXX]", "withOneCard"))
-  testsFor(checkView(new Deck(aceOfGolds :: twoOfCups :: Nil), SpanishCardView, "[XXXXXXXXX]", "withMoreThanOneCard"))
+  testsFor(checkView(emptyWaste, SpanishCardView, "[         ]", "withNoSpanishCards"))
+  testsFor(checkView(wasteWithCard(aceOfGolds), SpanishCardView, "[ 1 GOLDS ]", "withOneSpanishCard"))
+  testsFor(checkView(new Waste(aceOfGolds :: twoOfCups :: Nil), SpanishCardView, "[ 1 GOLDS ]", "withMoreThanOneSpanishCard"))
 
-  testsFor(checkView(emptyWaste, SpanishCardView, "[         ]", "withNoCards"))
-  testsFor(checkView(wasteWithCard(aceOfGolds), SpanishCardView, "[ 1 GOLDS ]", "withOneCard"))
-  testsFor(checkView(new Waste(aceOfGolds :: twoOfCups :: Nil), SpanishCardView, "[ 1 GOLDS ]", "withMoreThanOneCard"))
+  testsFor(checkView(emptyFoundation, SpanishCardView, "[         ]", "withNoSpanishCards"))
+  testsFor(checkView(foundationWithCard(aceOfGolds), SpanishCardView, "[ 1 GOLDS ]", "withOneSpanishCard"))
+  testsFor(checkView(new Foundation(twoOfGolds :: aceOfGolds :: Nil), SpanishCardView, "[ 2 GOLDS ]", "withMoreThanOneSpanishCard"))
 
-  testsFor(checkView(emptyFoundation, SpanishCardView, "[         ]", "withNoCards"))
-  testsFor(checkView(foundationWithCard(aceOfGolds), SpanishCardView, "[ 1 GOLDS ]", "withOneCard"))
-  testsFor(checkView(new Foundation(twoOfGolds :: aceOfGolds :: Nil), SpanishCardView, "[ 2 GOLDS ]", "withMoreThanOneCard"))
+  testsFor(checkView(emptyDeck, FrenchCardView, "[           ]", "withNoFrenchCards"))
+  testsFor(checkView(deckWithCard(aceOfHearts), FrenchCardView, "[XXXXXXXXXXX]", "withOneFrenchCard"))
+  testsFor(checkView(new Deck(aceOfHearts :: twoOfCups :: Nil), FrenchCardView, "[XXXXXXXXXXX]", "withMoreThanOneFrenchCard"))
+
+  testsFor(checkView(emptyWaste, FrenchCardView, "[           ]", "withNoFrenchCards"))
+  testsFor(checkView(wasteWithCard(aceOfHearts), FrenchCardView, "[ A HEARTS  ]", "withOneFrenchCard"))
+  testsFor(checkView(new Waste(aceOfHearts :: twoOfCups :: Nil), FrenchCardView, "[ A HEARTS  ]", "withMoreThanOneFrenchCard"))
+
+  testsFor(checkView(emptyFoundation, FrenchCardView, "[           ]", "withNoFrenchCards"))
+  testsFor(checkView(foundationWithCard(aceOfHearts), FrenchCardView, "[ A HEARTS  ]", "withOneFrenchCard"))
+  testsFor(checkView(new Foundation(twoOfHearts :: aceOfHearts :: Nil), FrenchCardView, "[ 2 HEARTS  ]", "withMoreThanOneFrenchCard"))
 }
